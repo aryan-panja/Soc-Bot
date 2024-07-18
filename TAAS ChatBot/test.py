dca908816404b2378f75229efa05cd722ec96f00
Aryan Panja <apanja_be22@thapar.edu>
2024-07-18 23:42:09 +0530

    gdsc bot

--- TAAS ChatBot/test.py
+++ TAAS ChatBot/test.py
@@ -1,35 +1,20 @@
 import google.generativeai as genai
 import json
 import streamlit as st
-import os
-import os
-from dotenv import load_dotenv
-
-from gtts import gTTS
-from io import BytesIO
-import IPython.display as ipd
-import tempfile
-import base64
-
-
-
-# Configure the API key (replace with your actual API key)
-# Load the environment variables from the .env file
-load_dotenv()
-
 
 
 # Get the API key from the environment variables
-genai.configure(api_key="AIzaSyBFuup2R6zanwAWnrQ6nKTsvuiTUzTm_0o")
+genai.configure(api_key="AIzaSyBvDqRGZRkBfEXHzedjlCyBH5nx11ETRA8")
 
 
 
 # Set up the model and load conversation history
 generation_config = {
-    "temperature": 0.9,
-    "top_p": 1,
-    "top_k": 1,
-    "max_output_tokens": 2048,
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+#   "response_mime_type": "text/plain",
 }
 
 safety_settings = [
@@ -40,29 +25,36 @@ safety_settings = [
 ]
 
 model = genai.GenerativeModel(
-    model_name="gemini-pro", generation_config=generation_config, safety_settings=safety_settings
+    model_name="gemini-1.5-flash", generation_config=generation_config, safety_settings=safety_settings
 )
 
-template = f"If you know the answer then give it in proper way and i f you don't know the answer then please don't give any answer or don't make it by yourself. And always say thank you at the end of every answer. Also ask to Ask me more queries and feel free to interect. Don't give any excess information from your own if you don'st know. Also give their social media handles if you have it don't make it by your own."
+# template = f"If you know the answer then give it in proper way and i f you don't know the answer then please don't give any answer or don't make it by yourself. And always say thank you at the end of every answer. Also ask to Ask me more queries and feel free to interect. Don't give any excess information from your own if you don'st know. Also give their social media handles if you have it don't make it by your own."
 
 # Loading Crops history for model
-with open("crops.json", "r") as f:
-    Crop_history = json.load(f)
 
-# Loading history for model
-with open("history.json", "r") as f:
+with open("soc.json", "r") as f:
     history = json.load(f)
 
+# with open("crops.json", "r") as f:
+#     Crop_history = json.load(f)
 
+# Loading history for model
+# with open("history.json", "r") as f:
+#     history = json.load(f)
 
 # Loading history for user
-with open("user_history.json", "r") as f:
-    uhistory = json.load(f)
+# with open("user_history.json", "r") as f:
+#     uhistory = json.load(f)
 
+# Loading history for model for students
+# with open("students.json", "r") as f:
+#     student_history = json.load(f)
 
+# with open("output.json", "r") as f:
+#     output = json.load(f)
 
 # Set up the UI
-st.title("TAAS BOT")
+st.title("GDSC BOT")
 st.header("Come and chat with Thapar's first ever Society Bot!")
 
 
@@ -74,31 +66,24 @@ st.write("<p style='color:red;'>This bot is currently in testing version on the 
 
 
 # Input and Generate Response Section
-st.subheader("Ask me Anything about my TAAS family!")
+st.subheader("Ask me Anything about my GDSC family!")
 input_prompt = st.text_area("Enter a prompt", key="input")
+template = f"{input_prompt}. If you know the answer then give it in proper way and if you don't know the answer then please don't give any answer or don't make it by yourself. Give proper and short and straight to the point answer as a summer semester chatbot. Also ask to Ask me more queries and feel free to interect. Don't give any excess information from your own if you don'st know. also don't give any excess information from your own if you don't know. Just stick to the question and context"
+# inputLLM = input_prompt + template
 generate_response = st.button("Generate Response")
 
 if generate_response:
+    # convo = model.start_chat(history=output, context=template, safety_settings=safety_settings)
     convo = model.start_chat(history=history)
-    response = convo.send_message(f"{input_prompt} + {template} ")
+    response = convo.send_message(input_prompt)
     st.subheader("Response is:")
     st.write(response.text)
 
-    # Convert text to voice using gTTS
-    tts = gTTS(text=response.text, lang='en')
-
-    # Save the audio to a temporary file
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp_audio:
-        tts.save(tmp_audio.name)
-
-    # Play the audio automatically using HTML audio element
-    st.markdown(f'<audio src="data:audio/mp3;base64,{base64.b64encode(open(tmp_audio.name, "rb").read()).decode()}" autoplay controls>', unsafe_allow_html=True)
-
     #add user history to separate json file
-    uhistory.append({"role": "user", "parts": [input_prompt]})
-    uhistory.append({"role": "model", "parts": [response.text]})
-    with open("user_history.json", "w") as f:
-        json.dump(uhistory, f)
+    # uhistory.append({"role": "user", "parts": [input_prompt]})
+    # uhistory.append({"role": "model", "parts": [response.text]})
+    # with open("user_history.json", "w") as f:
+    #     json.dump(uhistory, f)
 
 # Add to History Section
 st.subheader("Add to History")
@@ -114,6 +99,6 @@ if add_to_history:
     # Show success message
     st.success("Successfully added to history!")
 
-# Save updated history
-with open("history.json", "w") as f:
+# Save updated student history
+with open("soc.json", "w") as f:
     json.dump(history, f)
